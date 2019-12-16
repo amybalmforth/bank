ef8c24a6f71ba4f25b770146ac8f650eab9ef7d1
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-16 12:12:39 +0000

    can deposit 1000 pounds

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,13 +1,13 @@
 class Bank
+  attr_reader :balance
 
-attr_reader :balance
+  def initialize
+    @balance = 0
+  end
 
-def initialize
-  @balance = 0
-end
-
-
-
+  def deposit(amount)
+    @balance += 1000
+  end
 
 
 
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,13 +1,18 @@
-require "bank"
+require 'bank'
 
 describe Bank do
 
   describe "balance" do
-
-    it "start with a balance of 0" do
+    it "starts with a balance of 0" do
       expect(subject.balance).to eq 0
     end
+  end
 
+  describe "depositing" do
+    it "deposits 1000 pounds" do
+      subject.deposit(1000)
+      expect(subject.balance).to eq 1000
+    end
   end
 
 end
